4c184f17da7835a7d9969f1e03b84a008d76e7a1
Paru29 <59466619+Paru29@users.noreply.github.com>
2025-03-05 08:09:52 +0530

    first commitshjkkjhskjhjkdsads

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,31 +1,27 @@
-name: Deploy React App
+name: Deploys
 
 on:
   push:
-    branches:
-      - main  # Change branch if needed
+    branches: ["master"]
+  pull_request:
+    branches: ["master"]
 
 jobs:
-  build-and-deploy:
-    runs-on: ubuntu-latest
+  deploy:
+    runs-on: ubuntu-latest  # Specify the runner
 
     steps:
       - name: Checkout Repository
         uses: actions/checkout@v3
 
-      - name: Install Dependencies
-        run: npm install
-
-      - name: Build Project
-        run: npm run build
-
-      - name: Deploy to VPS
-        uses: appleboy/scp-action@master
+      - name: Deploy to Server
+        uses: appleboy/ssh-action@master
         with:
           host: ${{ secrets.HOST }}
           username: ${{ secrets.USERNAME }}
+          port: ${{ secrets.PORT }}
           key: ${{ secrets.SSHKEY }}
-          source: "dist/*"
-          target: "/var/www/html/socialshosha"
-
-      
+          script: |
+            cd /var/www/html/socialshosha || exit 1
+            chmod +x .scripts/deploy.sh
+            .scripts/deploy.sh
