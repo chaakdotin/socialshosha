a8dfe1f4380413de8bc06c115182a26d0510ae5e
Paru29 <59466619+Paru29@users.noreply.github.com>
2025-03-05 08:03:56 +0530

    s

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,32 +1,3 @@
-name: Deploys
-
-on:
-  push:
-    branches: ["master"]
-  pull_request:
-    branches: ["master"]
-
-jobs:
-  deploy:
-    runs-on: ubuntu-latest  # Specify the runner
-
-    steps:
-      - name: Checkout Repository
-        uses: actions/checkout@v3
-
-      - name: Deploy to Server
-        uses: appleboy/ssh-action@master
-        with:
-          host: ${{ secrets.HOST }}
-          username: ${{ secrets.USERNAME }}
-          port: ${{ secrets.PORT }}
-          key: ${{ secrets.SSHKEY }}
-          script: |
-            cd /var/www/html/socialshosha || exit 1
-            chmod +x .scripts/deploy.sh
-            .scripts/deploy.sh
-
-
 name: Deploy React App
 
 on:
