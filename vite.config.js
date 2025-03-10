487057b47a4b8660fa46e688ba345be0998cf792
Paru29 <59466619+Paru29@users.noreply.github.com>
2025-03-10 20:29:10 +0530

    historyApiFallback update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    historyApiFallback: true,
-    allowedHosts: ['breezy-breads-burn.loca.lt']
-  },
   plugins: [react()],
-
+  server: {
+    historyApiFallback: true
+  }
 })
