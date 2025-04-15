16d5a5a7c603220876149cdb2d9d4838148ad88e
Praveen Prasad <praveenpatkuwal29@gmail.com>
2025-04-15 19:43:31 +0530

    ojkhfjdf

--- src/ReactVideoCards.jsx
+++ src/ReactVideoCards.jsx
@@ -224,13 +224,16 @@ const ReactVideoCards = () => {
                     <div className='position-relative'>
                         <div className='px-3 py-3' style={{width:"100vw", height:"100vh"}}>
                             <div className='color-box'>
-                                <div className='col-12 meet_new_color'>
-                                    <span>Think.</span> 
-                                    <div className='d-flex d-none'>
-                                        <video src="https://videos.pexels.com/video-files/5752729/5752729-uhd_2560_1440_30fps.mp4" className='object-fit-cover'></video>
+                                
+                                <div className='col-12 new-div'>
+                                    <div className='col-12 meet_new_color'>
+                                        <span>Think.</span> 
+                                        <div className='d-flex d-none'>
+                                            <video src="https://videos.pexels.com/video-files/5752729/5752729-uhd_2560_1440_30fps.mp4" className='object-fit-cover'></video>
+                                        </div>
+                                        <span>Design.</span>
+                                        <span>Deliver.</span>
                                     </div>
-                                    <span>Design.</span>
-                                    <span>Deliver.</span>
                                 </div>
                                 <div className="scroll-down" style={{ textAlign: "center", paddingTop: "50px" }}>
                                     <p style={{ color: "#000", fontFamily: "sans-serif" }}>scroll down</p>
@@ -246,6 +249,7 @@ const ReactVideoCards = () => {
                                         ></div>
                                     </div>
                                 </div>
+                                <video src="/videos/videoplayback.mp4" muted autoPlay playsInline className='bg-video'></video>
                             </div>
                         </div>
                         
